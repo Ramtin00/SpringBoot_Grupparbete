4d5e05779c3baa8734ada2baee9d586e17c57f88
slyohmy <simon.westlin@hotmail.com>
2022-04-13 11:28:15 +0200

    Added controller Kund

---
+++ src/main/java/com/example/springboot_grupparbete/Controllers/KundController.java
@@ -0,0 +1,10 @@
+package com.example.springboot_grupparbete.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kund")
+public class KundController {
+
+}
