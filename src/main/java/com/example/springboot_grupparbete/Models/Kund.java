583fcc4258ecb04945010f8d2d8e46e294c05982
Filip Edfors <Filip.edfors@hotmail.com>
2022-04-13 11:11:21 +0200

    Created annotations for Class Kund

--- src/main/java/com/example/springboot_grupparbete/Models/Kund.java
+++ src/main/java/com/example/springboot_grupparbete/Models/Kund.java
@@ -1,7 +1,15 @@
 package com.example.springboot_grupparbete.Models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Kund {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String namn;
     private String address;
