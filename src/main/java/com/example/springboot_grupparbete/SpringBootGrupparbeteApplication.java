1f190a275b91af88a83b28ff11613c1693cbf9f0
Ramtin Pourshakour <ramtin_pourshakour@yahoo.com>
2022-04-13 10:14:03 +0200

    Initial

---
+++ src/main/java/com/example/springboot_grupparbete/SpringBootGrupparbeteApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_grupparbete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGrupparbeteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGrupparbeteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot_grupparbete/SpringBootGrupparbeteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_grupparbete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGrupparbeteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
