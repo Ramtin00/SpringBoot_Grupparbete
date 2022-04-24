ca94d33547325136d8b9d1ebd6b53c6509b43520
slyohmy <simon.westlin@hotmail.com>
2022-04-24 13:48:31 +0200

    Added endpoint tests to BeställningControllerTest class

--- src/test/java/com/example/springboot_grupparbete/Controllers/BeställningControllerTest.java
+++ src/test/java/com/example/springboot_grupparbete/Controllers/BeställningControllerTest.java
@@ -1,7 +1,11 @@
 package com.example.springboot_grupparbete.Controllers;
 
 import com.example.springboot_grupparbete.Models.Beställning;
+import com.example.springboot_grupparbete.Models.Kund;
+import com.example.springboot_grupparbete.Models.Produkt;
 import com.example.springboot_grupparbete.Repositories.BeställningRepository;
+import com.example.springboot_grupparbete.Repositories.KundRepository;
+import com.example.springboot_grupparbete.Repositories.ProduktRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +16,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.Arrays;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -29,21 +33,55 @@ public class BeställningControllerTest {
 
     @MockBean
     private BeställningRepository beställningRepository;
+    @MockBean
+    private KundRepository kundRepository;
+    @MockBean
+    private ProduktRepository produktRepository;
+
 
     @BeforeEach
     public void init(){
 
-        Beställning beställning = new Beställning();
-        when(beställningRepository.findById(1L)).thenReturn(Optional.of(beställning));
+        Beställning beställning1 = new Beställning();
+        Kund kund1 = new Kund("Simon","Nackademin","0703215621");
+        Produkt produkt1 = new Produkt("Calvin Klein","svart","46", 1000,1);
 
+        when(beställningRepository.findById(1L)).thenReturn(Optional.of(beställning1));
+        when(beställningRepository.findAll()).thenReturn(Arrays.asList(beställning1));
+
+        when(kundRepository.findById(1L)).thenReturn(Optional.of(kund1));
+        when(kundRepository.findAll()).thenReturn(Arrays.asList(kund1));
+
+        when(produktRepository.findById(1L)).thenReturn(Optional.of(produkt1));
+        when(produktRepository.findAll()).thenReturn(Arrays.asList(produkt1));
     }
 
+    //Beställning/post
     @Test
     void postBeställningTest() throws Exception {
         mvc.perform(MockMvcRequestBuilders.post("/beställning/post?kundId=1&produktId=1")
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("{\"id\":null,\"datum\":null,\"totalPris\":500,\"kund\":{\"id\":1,\"namn\":\"Stewart\",\"address\":\"Gatan 1\",\"telNr\":\"123\"},\"produkter\":[{\"id\":1,\"märke\":\"Adidas\",\"färg\":\"Röd\",\"storlek\":\"40\",\"pris\":500,\"antalILager\":14}]}")));
+                .andExpect(content().string(equalTo("{\"id\":null,\"datum\":null,\"totalPris\":1000,\"kund\":{\"id\":null,\"namn\":\"Simon\",\"address\":\"Nackademin\",\"telNr\":\"0703215621\"},\"produkter\":[{\"id\":null,\"mÃ¤rke\":\"Calvin Klein\",\"fÃ¤rg\":\"svart\",\"storlek\":\"46\",\"pris\":1000,\"antalILager\":0}]}")));
     }
+
+
+    //Beställning (findAll)
+    @Test
+    public void findAllBeställningTest() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/beställning")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[{\"id\":null,\"datum\":null,\"totalPris\":0,\"kund\":null,\"produkter\":[]}]"));
+    }
+
+    //Beställning/{id} (findById)
+    /*@Test
+    void getBeställningById() throws Exception{
+        mvc.perform(MockMvcRequestBuilders.get("/beställning/1")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"id\":null,\"datum\":null,\"totalPris\":0,\"kund\":null,\"produkter\":[]}"));
+    }*/
 }
 
